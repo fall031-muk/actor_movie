fe0e28ec26adc6e91c15cf94a9f9a2d65b198e7a
fall031 <fall900802@gmail.com>
2021-08-18 13:45:54 +0900

    Add: actor_movie setting

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'actor_movie',
+        'USER': 'root',
+        'PASSWORD': 'dbsgusanr12!A',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'django-insecure-ktgmbzokxd9&k*p&x3o-c220xe7=ayn6i2u3s8rjdmh*zkth%1'
